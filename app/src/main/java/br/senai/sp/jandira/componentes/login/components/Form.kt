cdcab110129f877c8da481034443427bd6708135
GENIVANIA MACEDO OLIVEIRA <22282083@dsn127m1241953.cfp127aluno.local>
2023-07-28 11:27:48 -0300

    OK

--- app/src/main/java/br/senai/sp/jandira/componentes/login/components/Form.kt
+++ app/src/main/java/br/senai/sp/jandira/componentes/login/components/Form.kt
@@ -1,37 +1,41 @@
 package br.senai.sp.jandira.componentes.login.components
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Card
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import br.senai.sp.jandira.componentes.Components.CaixaDeTexto
-import kotlin.math.log
+
 
 @Composable
 fun Form() {
-    var emailState = remember {
+    var emailState by remember {
         mutableStateOf("")
 
     }
+
+    var senhaState by remember {
+        mutableStateOf("")
+
+    }
+
     Card() {
         Column() {
             CaixaDeTexto(
                 texto = "E-mail",
                 meuType = KeyboardType.Email,
+                emailState,
                 aoDigitar = {
-                    emailState.value = it
+                    emailState = it
                 }
             )
             CaixaDeTexto(
                 texto = "Senha",
                 meuType = KeyboardType.Password,
+                senhaState,
                 aoDigitar = {
-
+                    senhaState = it
                 }
             )
 
